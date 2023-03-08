5ec42589b0ce0417b8a791d205b0f22a4f8b40e0
Simantob <e2295345@cmaisonneuve.qc.ca>
2023-03-08 10:51:54 -0500

    C11 début du carrousel

---
+++ js/carrousel.js
@@ -0,0 +1,6 @@
+
+(function()
+{
+    /*fonction IFEE */
+    console.log
+})
---
+++ sass/carrousel.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Extension carrousel - permet d'afficher ds une boite modale les images d'une galerie
+ * Package name: Carrousel
+ * Version: 1.0.0
+ */
+
+ /*
+ Plugin name: Carrousel
+ AUthor: Deborah Simantob
+ Plugin URI: https:github.com/eddytuto/carrousel 
+ Description: Cette extension permet d'afficher ds une boite modale les images d'une galerie avec un systeme de navigation
+ */
